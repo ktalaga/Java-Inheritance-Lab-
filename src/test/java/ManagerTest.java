706c81f99e9d557b390d39856aa712a0172e9151
konrad <ktalaga90@gmail.com>
2021-05-19 15:56:31 +0100

    TestManager class created

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,5 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.management.Manager;
+
 public class ManagerTest {
 
+    private Manager manager;
 
+    @Before
+    public void before(){
+        manager = new Manager("Konrad Talaga", "GG123456", 44000, "Testing");
+    }
+
+    @Test
+    public void hasName(){
+
+    }
 
 }
