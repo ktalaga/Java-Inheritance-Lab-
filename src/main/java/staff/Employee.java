5647ebc3955eb26759ce288e4de30a0b91059b66
konrad <ktalaga90@gmail.com>
2021-05-19 15:39:14 +0100

    employee parent class created

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,16 @@
+package staff;
+
+public class Employee {
+
+    private String name;
+    private String NINumber;
+    private int salary;
+
+    public Employee(String name, String NINumber, int salary ){
+        this.name = name;
+        this.NINumber = NINumber;
+        this.salary = salary;
+    }
+
+
+}
