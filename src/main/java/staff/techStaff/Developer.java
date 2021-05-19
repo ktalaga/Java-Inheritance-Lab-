20ea5450a7522f6365dac5cd76000ad06cd294e9
konrad <ktalaga90@gmail.com>
2021-05-19 16:27:26 +0100

    Developer, DeveloperTest classes added, 5 tests for developer passed

---
+++ src/main/java/staff/techStaff/Developer.java
@@ -0,0 +1,11 @@
+package staff.techStaff;
+
+import staff.Employee;
+
+public class Developer extends Employee {
+
+    public Developer(String name, String NINumber, int salary){
+        super(name, NINumber, salary);
+    }
+
+}
---
+++ src/test/java/DeveloperTest.java
@@ -0,0 +1,45 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeveloperTest {
+
+    private Developer developer;
+
+    @Before
+    public void before(){
+        developer = new Developer("John Mayer", "BB121212", 27000);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("John Mayer", developer.getName());
+    }
+
+    @Test
+    public void hasNINumber(){
+        assertEquals("BB121212", developer.getNINumber());
+    }
+
+    @Test
+    public void hasSalary(){
+        assertEquals(27000, developer.getSalary(),0.00);
+    }
+
+
+    @Test
+    public void canRaiseSalary(){
+        developer.raiseSalary(1200.50);
+        assertEquals(28200.5, developer.getSalary(), 0.00);
+    }
+
+    @Test
+    public void canPayBonus(){
+        developer.payBonus();
+        assertEquals(270, developer.payBonus(), 0.00);
+    }
+
+
+}
